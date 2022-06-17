c71304702da6bbe40b3f07964397851cb138c597
Jade0829 <bibe0829@gmail.com>
2022-06-17 10:48:27 +0900

    customized CSI

--- util/nvmf.go
+++ util/nvmf.go
@@ -102,6 +102,8 @@ func (node *nodeNVMf) CreateVolume(lvsName string, sizeMiB int64) (string, error
 		return "", fmt.Errorf("volume ID already exists: %s", lvolID)
 	}
 	node.lvols[lvolID] = &lvolNVMf{nsID: invalidNSID}
+	node.lovls[lvolID].model = lovlID
+	node.lvols[lvolID].nqn = "nqn.gluesys.csi:" + lvolID
 
 	klog.V(5).Infof("volume created: %s", lvolID)
 	return lvolID, nil
